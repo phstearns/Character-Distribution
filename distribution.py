8c5097e79e5655e92729dc87a2e1f200d9552873
phstearns <phstearns13@gmail.com>
2016-02-12 14:54:19 -0500

    Updated from Brython Server: 2/12/2016 2:54:20 PM

--- distribution.py
+++ distribution.py
@@ -37,8 +37,7 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 orig = str(input("Please enter a string of text (the bigger the better): "))
-print('The distribution of character in "' + u + '" is: ')
-i = orig.count('i')
+print('The distribution of character in "' + orig + '" is: ')
 alph = "abcdefghijklmnopqrstuvwxyz"
 for c in alph:
     print(orig.count(c))
