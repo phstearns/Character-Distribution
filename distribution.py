97734a891152bd57c3519d63c61abbedaee5468e
phstearns <phstearns13@gmail.com>
2016-02-22 12:36:49 -0500

    Updated from Brython Server: 2/22/2016 12:36:49 PM

--- distribution.py
+++ distribution.py
@@ -42,3 +42,29 @@ alph = "abcdefghijklmnopqrstuvwxyz"
 for c in alph:
     sort
     print(orig.count(c))
+    
+def compare(a, b):
+    """
+    compare - generic comparison function for testing two elements.
+    """
+    return b > a
+
+
+def bsort(seq, cmp):
+    """
+    bsort - simple sorting algorithm that uses any comparison function
+    seq - a list to be sorted
+    cmp - a function for comparing two elements of seq
+    """
+    sorted = False  # assume the seq is not sorted to start with
+    while not sorted:
+        sorted = True   # assume it's already sorted correctly
+        for index, value in enumerate(seq): # for every element in seq
+            if index > 0:                   # past the first..
+                if not cmp(seq[index-1], value):  # if this element is out of order
+                    sorted = False          # then the list is not sorted yet
+                    seq[index-1], seq[index] = seq[index], seq[index-1] # and swap it
+
+
+bsort(orig, compare)
+print(orig)
