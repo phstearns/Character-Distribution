73b4fe1ba294769b4ef1472600f01d56f5c87d94
phstearns <phstearns13@gmail.com>
2016-02-24 13:22:43 -0500

    Updated from Brython Server: 2/24/2016 1:22:44 PM

--- distribution.py
+++ distribution.py
@@ -41,7 +41,7 @@ def compare(a, b):
     """
     compare - generic comparison function for testing two elements.
     """
-    return b > a
+    return b == a
 
 
 o = str(input("Please enter a string of text (the bigger the better): "))
