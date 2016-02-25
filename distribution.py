0a1e2c06ec406a7fa9cc1f59e6554500afadad22
phstearns <phstearns13@gmail.com>
2016-02-25 14:19:32 -0500

    Updated from Brython Server: 2/25/2016 2:19:32 PM

--- distribution.py
+++ distribution.py
@@ -36,13 +36,7 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-"""
-def compare(a, b):
-    """
-    compare - generic comparison function for testing two elements.
-    """
-    return b > a
-"""
+
 dist={}
 o = str(input("Please enter a string of text (the bigger the better): "))
 print('The distribution of character in "' + o + '" is: ')
